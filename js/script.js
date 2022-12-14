3cc5c882bcd9a4fd806b269e419e0d9df6d8a428
AndreaMinelli <minelliandrea94@gmail.com>
2022-12-14 15:23:03 +0100

    Creato array toBuy

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-console.log("JS OK");
+/*
+Data una lista della spesa (inventatela), 
+stampare in pagina gli elementi della lista 
+individualmente con un ciclo while.  
+(anche brutalmente, basta che si vedano)
+*/
+
+//Targhettizzo gli elementi in pagina
+const expense = document.getElementById("expense");
+
+//Creo array
+const toBuy = ["uova", "latte", "formaggio", "pane"];
