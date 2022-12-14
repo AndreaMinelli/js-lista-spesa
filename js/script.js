9361861db401fdb938855e0061485487d04070b5
AndreaMinelli <minelliandrea94@gmail.com>
2022-12-14 15:49:21 +0100

    Stampata lista con metodo dei nodi

--- js/script.js
+++ js/script.js
@@ -5,11 +5,12 @@ individualmente con un ciclo while.
 (anche brutalmente, basta che si vedano)
 */
 
-//Targhettizzo gli elementi in pagina
-const expense = document.getElementById("expense");
-
 //Creo array
 const toBuy = ["uova", "latte", "formaggio", "pane"];
+
+// # TEMPLATE LITERALS
+//Targhettizzo gli elementi in pagina
+const expense = document.getElementById("expense");
 
 let listItem = "";
 
@@ -23,3 +24,25 @@ while (i < toBuy.length) {
 const list = `<ul>${listItem}</ul>`;
 
 expense.innerHTML = list;
+
+// # NODES
+//Targhettizzo gli elementi in pagina
+const expenseNode = document.getElementById("expense-node");
+
+//Creo elemento ul
+const listNode = document.createElement("ul");
+
+let j = 0;
+
+while (j < toBuy.length) {
+  //Creo elemento li
+  const itemNode = document.createElement("li");
+  //Inserisco elemento corrente array nel li
+  itemNode.append(toBuy[j]);
+  //Inserisco il li completo nell' ul
+  listNode.appendChild(itemNode);
+  j++;
+}
+
+//Inserisco l'ul nel DOM
+expenseNode.appendChild(listNode);
